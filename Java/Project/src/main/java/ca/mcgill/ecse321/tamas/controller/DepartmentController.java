b07c6ea3af29b72baaf42a807a992519b01feb3d
Tharsan <trp4145@bell.net>
2017-02-23 16:16:09 -0500

    Removed redundant methods from DepartmentController

--- Java/Project/src/main/java/ca/mcgill/ecse321/tamas/controller/DepartmentController.java
+++ Java/Project/src/main/java/ca/mcgill/ecse321/tamas/controller/DepartmentController.java
@@ -18,35 +18,7 @@ public class DepartmentController {
 
 	}
 
-	/**
-	 * @param courseStringInfo
-	 * @param courseIntegerInfo
-	 */
-	public void createCourse(String[] courseStringInfo, int[] courseIntegerInfo){
 
-		Course course = new Course(courseStringInfo[0],courseStringInfo[1], courseStringInfo[2], courseIntegerInfo[0], courseIntegerInfo[1],courseIntegerInfo[2],courseIntegerInfo[3],courseIntegerInfo[4],courseIntegerInfo[5],courseIntegerInfo[6],courseIntegerInfo[7],courseIntegerInfo[8],courseIntegerInfo[9]);
-		department.addAllCourse(course);
-		PersistenceXStream.saveToXMLwithXStream(department);
-
-
-	}
-
-	/**
-	 * @param Code
-	 * @param Name
-	 * @param Semester
-	 * @param NumberOfCredits
-	 * @param NumberOfLabs
-	 * @param NumberOfTutorials
-	 * @param NumberOfHours
-	 * @param StudentsEnrolled
-	 * @param TasNeeded
-	 * @param GradersNeeded
-	 * @param TaHourlyRate
-	 * @param GraderHourlyRate
-	 * @param Budget
-	 *
-	 */
 	public void createCourse(String code, String name, String semester, int numberOfCredits, int numberOfLabs, int numberOfTutorials, int numberOfHours, int studentsEnrolled, int tasNeeded, int gradersNeeded, int taHourlyRate, int graderHourlyRate, int budget){
 
 		Course course = new Course(code, name, semester, numberOfCredits, numberOfLabs, numberOfTutorials, numberOfHours, studentsEnrolled, tasNeeded, gradersNeeded, taHourlyRate, graderHourlyRate, budget);
@@ -56,19 +28,6 @@ public class DepartmentController {
 
 	}
 
-	/**
-	 * @param instructorStringInfo
-	 * @param instructorID
-	 *
-	 */
-	public void createInstructor(String[] instructorStringInfo, int instructorID){
-
-		Instructor instructor = new Instructor(instructorStringInfo[0], instructorID, instructorStringInfo[1]);
-		department.addAllInstructor(instructor);
-		PersistenceXStream.saveToXMLwithXStream(department);
-
-
-	}
 
 	/**
 	 * @param name
@@ -101,21 +60,6 @@ public class DepartmentController {
 		Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
 		department.addAllStudent(student);
 		PersistenceXStream.saveToXMLwithXStream(department);
-
-	}
-
-	/**
-	 * @param studentStringInfo
-	 * @param studentIntegerInfo
-	 * @param isGrad
-	 *
-	 */
-	public void createStudent(String[] studentStringInfo, int[] studentIntegerInfo, boolean isGrad){
-
-		Student student = new Student(studentIntegerInfo[0], studentStringInfo[0], studentStringInfo[1], isGrad, studentIntegerInfo[1], studentStringInfo[3], studentIntegerInfo[2]);
-		department.addAllStudent(student);
-		PersistenceXStream.saveToXMLwithXStream(department);
-
 
 	}
 
