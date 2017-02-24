4ff6a393165a66e7a7db908c3973ae29bf238d1f
shiyu-mochi <shiyu-mochi@users.noreply.github.com>
2017-02-24 12:58:08 -0500

    template

--- TAMASphp/CreateJobPosting.php
+++ TAMASphp/CreateJobPosting.php
@@ -8,4 +8,77 @@
 
 #Department page button
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="tabbable" id="tabs-212784">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a href="#panel-392363" data-toggle="tab">Section 1</a>
+					</li>
+					<li>
+						<a href="#panel-195771" data-toggle="tab">Section 2</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-392363">
+						<p>
+							I'm in Section 1.
+						</p>
+					</div>
+					<div class="tab-pane" id="panel-195771">
+						<p>
+							Howdy, I'm in Section 2.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-header">
+				<h1>
+					LayoutIt! <small>Interface Builder for Bootstrap</small>
+				</h1>
+			</div>
+			<form role="form">
+				<div class="form-group">
+					 
+					<label for="exampleInputEmail1">
+						Email address
+					</label>
+					<input type="email" class="form-control" id="exampleInputEmail1" />
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputPassword1">
+						Password
+					</label>
+					<input type="password" class="form-control" id="exampleInputPassword1" />
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputFile">
+						File input
+					</label>
+					<input type="file" id="exampleInputFile" />
+					<p class="help-block">
+						Example block-level help text here.
+					</p>
+				</div>
+				<div class="checkbox">
+					 
+					<label>
+						<input type="checkbox" /> Check me out
+					</label>
+				</div> 
+				<button type="submit" class="btn btn-default">
+					Submit
+				</button>
+			</form>
+		</div>
+	</div>
+</div>
+
 php>
