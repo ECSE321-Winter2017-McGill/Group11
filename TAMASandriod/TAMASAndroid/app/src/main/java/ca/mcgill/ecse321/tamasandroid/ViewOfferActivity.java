d2508491f1a765d4854611e8d6239a14b912d4ad
edwardtanzhao <edwardtanzhao@gmail.com>
2017-03-20 15:24:36 -0400

    Created android project with updated view
    Created a new project that will have the updated view

---
+++ TAMASandriod/TAMASAndroid/app/src/main/java/ca/mcgill/ecse321/tamasandroid/ViewOfferActivity.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.tamasandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewOfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_offer);
+
+        //code for display the jobs
+    }
+}
