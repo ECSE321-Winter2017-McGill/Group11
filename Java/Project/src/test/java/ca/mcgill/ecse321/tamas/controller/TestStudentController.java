3bdd732ab2efe9a7201ba92b78616e64456b1579
edwardtanzhao <edward.zhao@mail.cmgill.ca>
2017-03-30 22:32:13 -0400

    added test for respond to offer
    added test for respond to offer, can clean up code by added a method due
    to repitition

--- Java/Project/src/test/java/ca/mcgill/ecse321/tamas/controller/TestStudentController.java
+++ Java/Project/src/test/java/ca/mcgill/ecse321/tamas/controller/TestStudentController.java
@@ -196,5 +196,78 @@ public class TestStudentController {
         assertEquals(1, jobPosting.getApplicant().size());
 	}
 	
+	@Test
+	public void testRespondToOffer(){
+		assertEquals(0, department.getAllStudents().size());
+		assertEquals(0, department.getAllJobs().size());
+        assertEquals(0, department.getAllCourses().size());
+        assertEquals(0, department.getAllInstructors().size());
+        
+        StudentController studentController = new StudentController(department);
+
+		
+		//Create Student
+		int studentID = 123456789;
+		String name = "Jonathan";
+		String email = "jonathan@mcgill.ca";
+		boolean isGrad = true;
+		int year = 2016;
+		String jobPreference = "Preference to be a TA";
+		int numberOfHours = 16;
+		
+		Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
+		department.addAllStudent(student);
+		assertEquals(1, department.getAllStudents().size());
+		
+		//Create Instructor
+        String instructorName = "James";
+        int instructorID = 12345;
+        String instructorEmail = "james@mcgill.ca";
+
+        Instructor instructor = new Instructor(instructorName,instructorID,instructorEmail);
+        department.addAllInstructor(instructor);
+        assertEquals(1, department.getAllInstructors().size());
+        
+		//Create Course
+        String courseCode = "ECSE321";
+        String courseName = "Software Engineering";
+        String courseSemester = "W2017";
+        int courseNumOfCredits = 3;
+        int courseNumOfLabs = 0;
+        int courseNumOfTutorials = 2;
+        int courseNumOfHours = 1;
+        int courseNumOfStudentsEnrolled = 100;
+        int courseTasRequired = 1;
+        int courseGradersRequired = 1;
+        int courseTaHourlyRates = 12;
+        int courseGraderHourlyRates = 12;
+        int courseBudget = 10000;
+
+        Course course = new Course(courseCode,courseName,courseSemester,courseNumOfCredits,courseNumOfLabs,courseNumOfTutorials,courseNumOfHours,courseNumOfStudentsEnrolled,courseTasRequired,courseGradersRequired,courseTaHourlyRates,courseGraderHourlyRates,courseBudget,instructor);
+        department.addAllCourse(course);
+        assertEquals(1, department.getAllCourses().size());
+
+        //Create Job
+        PositionType posType = PositionType.Grader;
+        Calendar c = Calendar.getInstance();
+        c.set(2017, Calendar.MARCH, 16, 9, 0, 0);
+        Date postDeadLine = new Date(c.getTimeInMillis());
+
+        Job jobOffer = new Job(posType,postDeadLine, course);
+        department.addAllJob(jobOffer);
+        assertEquals(1, department.getAllJobs().size());
+        
+        //accept or decline
+        boolean accept = true;
+        
+        assertEquals(0, jobOffer.getEmployee().size());
+        
+        studentController.respondToJobOffer(student, jobOffer, accept);
+        
+        assertEquals(1, jobOffer.getEmployee().size());
+        
+		
+	}
+	
 
 }
