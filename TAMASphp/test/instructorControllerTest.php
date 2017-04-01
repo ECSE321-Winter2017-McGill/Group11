1d54024f16419231ff4ae0503bdc5aa51f57f757
ArenBabikian <aren.babikian@mail.mcgill.ca>
2017-04-01 12:30:20 -0400

    Implemented initial tests
    Not functional

--- TAMASphp/test/instructorControllerTest.php
+++ TAMASphp/test/instructorControllerTest.php
@@ -19,7 +19,7 @@ class instructorControllerTest extends PHPUnit_Framework_TestCase
         $this->contr = new InstructorController();
         $this->persis = new PersistenceTamas();
         $this->dpt = $this->persis->loadDataFromStore();
-        $this->dpt->delete();
+        //$this->dpt->delete();
         $this->persis->writeDataToStore($this->dpt);
     }
 	
@@ -28,82 +28,105 @@ class instructorControllerTest extends PHPUnit_Framework_TestCase
     }
     
     public function testCreateInstructor(){
+    	$this->assertEquals(0, count($this->dpt->getAllInstructors()));
     	
+    	$name = "Daniel Varro";
+    	$id = "123456789";
+    	$email = "daniel.varro@mail.mcgill.ca";
+    	
+    	try{
+    		$this->contr->createInstructor($name, $id, $email);
+    	} catch (Exception $e) {
+    		$this->fail();
+    	}
+    	
+    	$this->dpt = $this->persis->loadDataFromStore();
+    	$this->assertEquals(0, count($this->dpt->getAllCourses()));
+    	$this->assertEquals(0, count($this->dpt->getAllJobs()));
+    	$this->assertEquals(0, count($this->dpt->getAllReviews()));
+    	$this->assertEquals(0, count($this->dpt->getAllStudents()));
+    	$this->assertEquals(1, count($this->dpt->getAllInstructors()));
+    	
+    	$this->assertEquals(0, count($this->dpt->getAllInstructor_index(0)->getCourses()));
+    	$this->assertEquals(0, count($this->dpt->getAllInstructor_index(0)->getReviewText()));
+    	$this->assertEquals($name, $this->dpt->getAllInstructor_index(0)->getName());
+    	$this->assertEquals($id, $this->dpt->getAllInstructor_index(0)->getInstructorID());
+    	$this->assertEquals($email, $this->dpt->getEvent_index(0)->getEmail());    	
     }
     
     public function testCreateInstructorNull(){
-    	 
+    	 $this->pass();
     }
     
     public function testCreateInstructorEmpty(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateInstructorSpace(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateInstructorInvalidFormats(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateJobPosting(){
-    	
+    	$this->pass();
     }
     
     public function testCreateJobPostingNull(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateJobPostingEmpty(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateJobPostingSpace(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateJobPostingOfferDeadlineDateBeforePostingDeadlineDate(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateJobPostingJobInexistant(){
-    	 
+    	$this->pass();
     }
     
     public function testmodyifyAllocation(){
-    	
+    	$this->pass();
     }
     
     public function testmodyifyAllocationNull(){
-    	 
+    	$this->pass();
     }
     
     public function testmodyifyAllocationJobInexistant(){
-    	 
+    	$this->pass();
     }
     
     public function testmodyifyAllocationStudentsInexistant(){
-    
+    	$this->pass();
     }
     
     public function testCreateReview(){
-    	 
+    	$this->pass();
     }
     
     public function testCreateReviewNull(){
-    
+    	$this->pass();
     }
     
     public function testCreateReviewEmptyContent(){
-    
+    	$this->pass();
     }
     
     public function testCreateReviewSpaceContent(){
-    
+    	$this->pass();
     }
     
     public function testCreateReviewInstructorJobAndStudentInexistant(){
-    
+    	$this->pass();
     }
 }
