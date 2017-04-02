551c678d2b1e25fc18d0dff059246e60a774a142
matlapo <mathieu.lapointe3@mail.mcgill.ca>
2017-04-02 17:41:24 -0400

    Gradle issues fixed.

--- Java/Project/src/test/java/ca/mcgill/ecse321/tamas/controller/TestStudentController.java
+++ Java/Project/src/test/java/ca/mcgill/ecse321/tamas/controller/TestStudentController.java
@@ -16,73 +16,76 @@ import ca.mcgill.ecse321.tamas.model.*;
 import ca.mcgill.ecse321.tamas.persistence.PersistenceXStream;
 
 /**
- * 
+ *
  * Created by edwardtanzhao on 30/3/2017
  *
  */
 public class TestStudentController {
-	
-	private Department department;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		PersistenceXStream.initializeModelManager("output"+File.separator+"data.xml");
-	}
+    private Department department;
 
-	@Before
-	public void setUp() throws Exception {
-		department = new Department();
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        PersistenceXStream.initializeModelManager("output"+File.separator+"data.xml");
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		department.delete();
-	}
+    @Before
+    public void setUp() throws Exception {
+        department = new Department();
+    }
 
-	@Test
-	public void testCreateStudent() {
-		assertEquals(0, department.getAllStudents().size());
-		
-		StudentController studentController = new StudentController(department);
-		
-		//create student
-		int studentID = 123456789;
-		String name = "Jonathan";
-		String email = "jonathan@mcgill.ca";
-		boolean isGrad = true;
-		int year = 2016;
-		String jobPreference = "Preference to be a TA";
-		int numberOfHours = 16;
-		
-		studentController.createStudent(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
-		
-		assertEquals(1, department.getAllStudents().size());
-	}
-	
-	@Test
-	public void testAddPrevJobExp() {
-		assertEquals(0, department.getAllStudents().size());
-		assertEquals(0, department.getAllJobs().size());
+    @After
+    public void tearDown() throws Exception {
+        department.delete();
+    }
+
+    @Test
+    public void testCreateStudent() {
+        assertEquals(0, department.getAllStudents().size());
+
+        StudentController studentController = new StudentController(department);
+
+        //create student
+        int studentID = 123456789;
+        String name = "Jonathan";
+        String email = "jonathan@mcgill.ca";
+        boolean isGrad = true;
+        int year = 2016;
+        String jobPreference = "Preference to be a TA";
+        int numberOfHours = 16;
+
+        Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
+        department.addAllStudent(student);
+
+        assertEquals(1, department.getAllStudents().size());
+
+        student.delete();
+    }
+
+    @Test
+    public void testAddPrevJobExp() {
+        assertEquals(0, department.getAllStudents().size());
+        assertEquals(0, department.getAllJobs().size());
         assertEquals(0, department.getAllCourses().size());
         assertEquals(0, department.getAllInstructors().size());
-        
+
         StudentController studentController = new StudentController(department);
-		
-		//Create Student
-		int studentID = 123456789;
-		String name = "Jonathan";
-		String email = "jonathan@mcgill.ca";
-		boolean isGrad = true;
-		int year = 2016;
-		String jobPreference = "Preference to be a TA";
-		int numberOfHours = 16;
-		
-		Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
-		department.addAllStudent(student);
-		assertEquals(1, department.getAllStudents().size());
-		assertEquals(0, student.getPreviousJobExperiences());
-		
-		//Create Instructor
+
+        //Create Student
+        int studentID = 123456789;
+        String name = "Jonathan";
+        String email = "jonathan@mcgill.ca";
+        boolean isGrad = true;
+        int year = 2016;
+        String jobPreference = "Preference to be a TA";
+        int numberOfHours = 16;
+
+        Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
+        department.addAllStudent(student);
+        assertEquals(1, department.getAllStudents().size());
+        assertEquals(0, student.getPreviousJobExperiences().size());
+
+        //Create Instructor
         String instructorName = "James";
         int instructorID = 12345;
         String instructorEmail = "james@mcgill.ca";
@@ -90,8 +93,8 @@ public class TestStudentController {
         Instructor instructor = new Instructor(instructorName,instructorID,instructorEmail);
         department.addAllInstructor(instructor);
         assertEquals(1, department.getAllInstructors().size());
-        
-		//Create Course
+
+        //Create Course
         String courseCode = "ECSE321";
         String courseName = "Software Engineering";
         String courseSemester = "W2017";
@@ -119,36 +122,39 @@ public class TestStudentController {
         Job job = new Job(posType,postDeadLine, course);
         department.addAllJob(job);
         assertEquals(1, department.getAllJobs().size());
-        
+
         //add experience to student
         studentController.addPreviousExperienceToStudent(student, job);
-        assertEquals(1, student.getPreviousJobExperiences());
-	}
-	
-	@Test
-	public void applyToJobPosting(){
-		assertEquals(0, department.getAllStudents().size());
-		assertEquals(0, department.getAllJobs().size());
+        assertEquals(1, student.getPreviousJobExperiences().size());
+
+        student.delete();
+        instructor.delete();
+    }
+
+    @Test
+    public void applyToJobPosting(){
+        assertEquals(0, department.getAllStudents().size());
+        assertEquals(0, department.getAllJobs().size());
         assertEquals(0, department.getAllCourses().size());
         assertEquals(0, department.getAllInstructors().size());
-        
+
         StudentController studentController = new StudentController(department);
 
-		
-		//Create Student
-		int studentID = 123456789;
-		String name = "Jonathan";
-		String email = "jonathan@mcgill.ca";
-		boolean isGrad = true;
-		int year = 2016;
-		String jobPreference = "Preference to be a TA";
-		int numberOfHours = 16;
-		
-		Student applicant = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
-		department.addAllStudent(applicant);
-		assertEquals(1, department.getAllStudents().size());
-		
-		//Create Instructor
+
+        //Create Student
+        int studentID = 123456789;
+        String name = "Jonathan";
+        String email = "jonathan@mcgill.ca";
+        boolean isGrad = true;
+        int year = 2016;
+        String jobPreference = "Preference to be a TA";
+        int numberOfHours = 16;
+
+        Student applicant = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
+        department.addAllStudent(applicant);
+        assertEquals(1, department.getAllStudents().size());
+
+        //Create Instructor
         String instructorName = "James";
         int instructorID = 12345;
         String instructorEmail = "james@mcgill.ca";
@@ -156,8 +162,8 @@ public class TestStudentController {
         Instructor instructor = new Instructor(instructorName,instructorID,instructorEmail);
         department.addAllInstructor(instructor);
         assertEquals(1, department.getAllInstructors().size());
-        
-		//Create Course
+
+        //Create Course
         String courseCode = "ECSE321";
         String courseName = "Software Engineering";
         String courseSemester = "W2017";
@@ -186,40 +192,43 @@ public class TestStudentController {
         department.addAllJob(jobPosting);
         assertEquals(1, department.getAllJobs().size());
         assertEquals(0, jobPosting.getApplicant().size());
-        
+
         try{
-        	studentController.applyToJobPosting(jobPosting, applicant);
+            studentController.applyToJobPosting(jobPosting, applicant);
         } catch (InvalidInputException e){
             fail();
         }
-        
+
         assertEquals(1, jobPosting.getApplicant().size());
-	}
-	
-	@Test
-	public void testRespondToOffer(){
-		assertEquals(0, department.getAllStudents().size());
-		assertEquals(0, department.getAllJobs().size());
+
+        applicant.delete();
+        instructor.delete();
+    }
+
+    @Test
+    public void testRespondToOffer(){
+        assertEquals(0, department.getAllStudents().size());
+        assertEquals(0, department.getAllJobs().size());
         assertEquals(0, department.getAllCourses().size());
         assertEquals(0, department.getAllInstructors().size());
-        
+
         StudentController studentController = new StudentController(department);
 
-		
-		//Create Student
-		int studentID = 123456789;
-		String name = "Jonathan";
-		String email = "jonathan@mcgill.ca";
-		boolean isGrad = true;
-		int year = 2016;
-		String jobPreference = "Preference to be a TA";
-		int numberOfHours = 16;
-		
-		Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
-		department.addAllStudent(student);
-		assertEquals(1, department.getAllStudents().size());
-		
-		//Create Instructor
+
+        //Create Student
+        int studentID = 123456789;
+        String name = "Jonathan";
+        String email = "jonathan@mcgill.ca";
+        boolean isGrad = true;
+        int year = 2016;
+        String jobPreference = "Preference to be a TA";
+        int numberOfHours = 16;
+
+        Student student = new Student(studentID, name, email, isGrad, year, jobPreference, numberOfHours);
+        department.addAllStudent(student);
+        assertEquals(1, department.getAllStudents().size());
+
+        //Create Instructor
         String instructorName = "James";
         int instructorID = 12345;
         String instructorEmail = "james@mcgill.ca";
@@ -227,8 +236,8 @@ public class TestStudentController {
         Instructor instructor = new Instructor(instructorName,instructorID,instructorEmail);
         department.addAllInstructor(instructor);
         assertEquals(1, department.getAllInstructors().size());
-        
-		//Create Course
+
+        //Create Course
         String courseCode = "ECSE321";
         String courseName = "Software Engineering";
         String courseSemester = "W2017";
@@ -256,18 +265,19 @@ public class TestStudentController {
         Job jobOffer = new Job(posType,postDeadLine, course);
         department.addAllJob(jobOffer);
         assertEquals(1, department.getAllJobs().size());
-        
+
         //accept or decline
         boolean accept = true;
-        
+
         assertEquals(0, jobOffer.getEmployee().size());
-        
+
         studentController.respondToJobOffer(student, jobOffer, accept);
-        
+
         assertEquals(1, jobOffer.getEmployee().size());
-        
-		
-	}
-	
+
+        student.delete();
+        instructor.delete();
+    }
+
 
 }
